53549e944b2e474b0e72006db8248408b79112f4
Francis Gauthier <frankigauthier93@gmail.com>
2016-08-19 10:32:17 -0400

    Commit mise à jour.

--- test/integration_test/facade-punctuation.js
+++ test/integration_test/facade-punctuation.js
@@ -14,24 +14,30 @@ JSrealLoader({
         assert.equal( C(","), ",", "6. Conjonction" );
         
         // before
-        assert.equal( S(N("cadeau").n("p")).b("-").cap(), "-Cadeaux.", "Tiret en début de phrase" );
-        assert.equal( S(N("cadeau").n("p")).b("*").cap(), "*Cadeaux.", "Etoile en début de phrase" );
-        assert.equal( S(N("cadeau").n("p")).b(".").cap(), ". Cadeaux.", "Point en début de phrase" );
+        assert.equal( S(N("cadeau").n("p")).b("-"), "-Cadeaux.", "Tiret en début de phrase" );
+        assert.equal( S(N("cadeau").n("p")).b("*"), "*Cadeaux.", "Etoile en début de phrase" );
+        assert.equal( S(N("cadeau").n("p")).b("."), ". Cadeaux.", "Point en début de phrase" );
 
         // after
-        assert.equal( S(N("cadeau").n("p")).a(",").cap(), "Cadeaux,", "1 virgule d'exclamation en fin de phrase" );
-        assert.equal( S(N("cadeau").n("p")).a("!").cap(), "Cadeaux!", "1 point d'exclamation en fin de phrase" );
-        assert.equal( S(N("cadeau").n("p")).a("...").cap(), "Cadeaux...", "3 points en fin de phrase" );
-        assert.equal( S(N("cadeau").n("p").a("..."), V("dormir").pe(3), V("manger").pe(1)).cap(), "Cadeaux... dort mange.", "Ponctuation sur des éléments à l'intérieur de la phrase" );
+        assert.equal( S(N("cadeau").n("p")).a(","), "Cadeaux,", "1 virgule d'exclamation en fin de phrase" );
+        assert.equal( S(N("cadeau").n("p")).a("!"), "Cadeaux!", "1 point d'exclamation en fin de phrase" );
+        assert.equal( S(N("cadeau").n("p")).a("..."), "Cadeaux...", "3 points en fin de phrase" );
+        assert.equal( S(N("cadeau").n("p").a("..."), V("dormir").pe(3), V("manger").pe(1)), "Cadeaux... dort mange.", "Ponctuation sur des éléments à l'intérieur de la phrase" );
         
         // surround
-        assert.equal( S(N("cadeau").n("p")).en("(").cap(), "(Cadeaux)", "Entourer la phrase de parenthèses" );
-        assert.equal( S(N("cadeau").n("p")).en(")").cap(), "(Cadeaux)", "Entourer la phrase de parenthèses" );
-        assert.equal( S(N("cadeau").n("p")).a("!").en("[").cap(), "[Cadeaux!]", "Entourer la phrase de crochets" );
-        assert.equal( S(N("cadeau").n("p")).a("!").en("]").cap(), "[Cadeaux!]", "Entourer la phrase de crochets" );
-        assert.equal( S(A("grand").g("f"), N("maison"), N("cadeau").n("p").a("!").en("]")).cap(), "Grande maison [cadeaux!].", "Entourer la phrase de crochets" );
+        assert.equal( S(N("cadeau").n("p")).en("("), "(Cadeaux)", "Entourer la phrase de parenthèses" );
+        assert.equal( S(N("cadeau").n("p")).en(")"), "(Cadeaux)", "Entourer la phrase de parenthèses" );
+        assert.equal( S(N("cadeau").n("p")).a("!").en("["), "[Cadeaux!]", "Entourer la phrase de crochets" );
+        assert.equal( S(N("cadeau").n("p")).a("!").en("]"), "[Cadeaux!]", "Entourer la phrase de crochets" );
+        assert.equal( S(A("grand").g("f"), N("maison"), N("cadeau").n("p").a("!").en("]")), "Grande maison [cadeaux!].", "Entourer la phrase de crochets" );
 
-        //liaison forcée
+        // capitalisation
+        assert.equal( NP(D("le").cap(),N("souris")), "La souris", "Première lettre en majuscule");
+        assert.equal( NP(D("le"),N("souris").cap(),A("géant")), "la Souris géante", "Lettre en majuscule en milieu de phrase");
+        assert.equal( S(NP(D("le"),N("souris"),A("géant"))), "La souris géante.", "Première lettre en majuscule automatique");
+        assert.equal( S(NP(D("le").cap(),N("souris"),A("géant"))), "La souris géante.", "Première lettre en majuscule automatique");
+
+        // liaison forcée
         assert.equal( S(VP( V("rejoindre").t("ip").pe("2").lier(), Pro("le"))), "Rejoins-le.", "Trait d'union entre les mots");
         assert.equal( S(VP( V("rejoindre").t("ip").lier(), Pro("le"))), "Rejoins-le.", "Trait d'union entre les mots");
         assert.equal( S(NP(D("le").lier(),N("horloge"))),"La-horloge.", "Trait d'union empêchant l'élision");
