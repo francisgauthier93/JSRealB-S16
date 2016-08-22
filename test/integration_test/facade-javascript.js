ff29df99fd48d129413a280471dc091059cc9aed
francisgauthier93 <frankigauthier93@gmail.com>
2016-08-22 09:11:31 -0400

    Ajout facade-javascript

---
+++ test/integration_test/facade-javascript.js
@@ -0,0 +1,50 @@
+var pom = NP(D("le"),N("pomme"));
+var gars = NP(D('le'),N('garçon')).n('p');
+var aime = V('aimer');
+
+var apple = NP(D("the"),N("apple"));
+var gars = NP(D('a'),N('boy'));
+var aime = V('love');
+
+JSrealLoader({
+        language: "fr",
+        lexiconUrl: URL.lexicon.fr,
+        ruleUrl: URL.rule.fr,
+        featureUrl: URL.feature
+    }, function() {
+    QUnit.test( "JavaScript FR", function( assert ) {
+        assert.equal(pom,'la pomme', 'Utilisation de var');
+        assert.equal(pom.clone(),'la pomme', 'Utilisation de var + clone');
+        assert.equal(pom.clone().n('p'),'les pommes', 'Utilisation de var + clone + transformations');
+        assert.equal(aime.clone().t('f'),'aimera', 'Utilisation de var + clone + transformations');
+        assert.equal(pom,'la pomme', 'Utilisation de var');
+        assert.equal(aime, 'aime','Utilisation de var');
+
+        //ADD
+        assert.equal(S.add(pom),'la pomme', 'add')
+        assert.equal(CP(C('et'),NP(D('le'),N('fruit'))).add(pom).add(gars),'le fruit, la pomme et les garçons', 'add (2 fois)');
+        assert.equal(S(VP().add(aime).add(pom)).add(gars,0),'Les garçons aiment la pomme.', 'add 3 fois + position');
+   });
+
+    JSrealLoader({
+            language: "en",
+            lexiconUrl: URL.lexicon.en,
+            ruleUrl: URL.rule.en,
+            featureUrl: URL.feature
+        }, function() {
+        QUnit.test( "JavaScript EN", function( assert ) {
+            assert.equal(apple,'the apple', 'Utilisation de var');
+            assert.equal(apple.clone(),'the apple', 'Utilisation de var + clone');
+            assert.equal(apple.clone().n('p'),'the apples', 'Utilisation de var + clone + transformations');
+            assert.equal(love.clone().t('f'),'will love', 'Utilisation de var + clone + transformations');
+            assert.equal(apple,'the apple', 'Utilisation de var');
+            assert.equal(love, 'love','Utilisation de var');
+
+            //ADD
+            assert.equal(S.add(apple),'the apple', 'add')
+            assert.equal(CP(C('and'),NP(D('a'),N('fruit'))).add(apple).add(boy),'a fruit, the apple and the boy', 'add (2 fois)');
+            assert.equal(S(VP().add(love).add(apple)).add(boy,0),'The boy loves the apple.', 'add 3 fois + position');
+
+        });
+    });
+});
